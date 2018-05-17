526141f0b9c06d40375110704f842e03cb9c38fe
CharlesCarbonne <charlescarbonne@hotmail.fr>
2018-05-17 11:17:58 +0200

    update

--- src/main/java/hello/PdfGeneratorTasklet.java
+++ src/main/java/hello/PdfGeneratorTasklet.java
@@ -8,8 +8,6 @@ import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.InitializingBean;
-import com.itextpdf.licensekey.LicenseKey;
-import poc.testItext.batch.utils.HtmlToPdf;
 
 public class PdfGeneratorTasklet implements Tasklet, InitializingBean {
 
@@ -18,13 +16,12 @@ public class PdfGeneratorTasklet implements Tasklet, InitializingBean {
 	/** The path to the source HTML file. */
 	public static final String SRC = String.format("%shello.html", BASEURI);
 	/** The target folder for the result. */
-	public static final String TARGET = "resources/results/generatedPdf/";
+	public static final String TARGET = "src/main/resources/results/generatedPdf/";
 	/** The path to the resulting PDF file. */
 	public static final String DEST = String.format("%stest.pdf", TARGET);
 
 	@Override
 	public RepeatStatus execute(StepContribution arg0, ChunkContext arg1) throws Exception {
-		LicenseKey.loadLicenseFile("itextkey-523953391946_0.xml");
 		File file = new File(TARGET);
 		file.mkdirs();
 		HtmlToPdf htpdf = new HtmlToPdf();
