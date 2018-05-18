3a65247951915a74d26fe1f8ea9bf310080fd92b
CharlesCarbonne <charlescarbonne@hotmail.fr>
2018-05-18 17:11:09 +0200

    update

--- src/main/java/hello/PdfGeneratorTasklet.java
+++ src/main/java/hello/PdfGeneratorTasklet.java
@@ -2,8 +2,6 @@ package hello;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,22 +10,16 @@ import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Value;
 
 public class PdfGeneratorTasklet implements Tasklet, InitializingBean {
 
 	/** The Base URI of the HTML page. */
 	public static final String BASEURI = "src/main/resources/html/";
-	/** The path to the source HTML file. */
-	public static final String SRC = String.format("%shello.html", BASEURI);
 	/** The target folder for the result. */
 	public static final String TARGET = "src/main/resources/results/generatedPdf/";
-	/** The path to the resulting PDF file. */
+
 	
 	File file = new File(BASEURI);
-
-	public String HTML = this.getHtmlFileToString(file);
-	
 	File folder = new File("/home/user/LEGIFRANCE/itextPoc/src/main/resources/html/");
 	File[] listOfFiles = folder.listFiles();
 	
@@ -44,16 +36,6 @@ public List<String> getListofFilesNames(File[] files) {
 	}
 	return filesNames;
 }
-
-
-
-//TODO créer liste de Strings HTML pour conversion.
-//modifier le repeatStatus pour qu'il traite la liste de strings html
-	
-
-	@Value("${test.value}")
-	private String test;
-
 	@Override
 	public RepeatStatus execute(StepContribution arg0, ChunkContext arg1) throws Exception {
 		this.getListofFilesNames(listOfFiles);
@@ -70,10 +52,7 @@ public List<String> getListofFilesNames(File[] files) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
 		}
-		
-
 return RepeatStatus.FINISHED;
 	}
 
@@ -81,25 +60,5 @@ return RepeatStatus.FINISHED;
 	public void afterPropertiesSet() throws Exception {
 		// TODO Auto-generated method stub
 
-	}
-
-	public String getHtmlFileToString(File path) {
-		List<String> lines;
-		String contents = null;
-		try {
-			lines = Files.readAllLines(Paths.get("src/main/resources/html/hello.html"));
-			StringBuilder sb = new StringBuilder();
-			for (String line : lines) {
-				if (sb.length() > 0) {
-					sb.append("\n");
-				}
-				sb.append(line);
-			}
-			contents = sb.toString();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return contents;
 	}
 }
